971248f14bc4de2ab573a5a398c3817c915f6eae
Pawel Zubkiewicz <pzubkiewicz@ptc.com>
2021-04-17 17:25:24 +0200

    chore: axios config moved outside from a test

--- __tests__/createItem/function.int.js
+++ __tests__/createItem/function.int.js
@@ -1,9 +1,10 @@
 const { default: axios } = require('axios')
 
+axios.defaults.baseURL = `https://${process.env.httpApiGatewayEndpointId}.execute-api.${process.env.region}.amazonaws.com`
+
 describe('createItem function', () => {
   it('should respond with statusCode 200 to correct request', async () => {
     // GIVEN
-    axios.defaults.baseURL = `https://${process.env.httpApiGatewayEndpointId}.execute-api.${process.env.region}.amazonaws.com`
     const payload = {}
 
     // WHEN
