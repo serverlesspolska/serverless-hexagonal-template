ed4e073ab0a885318d9c60a5ec5baab29a72039b
Pawel Zubkiewicz <pzubkiewicz@ptc.com>
2021-04-18 19:57:37 +0200

    feat: add lambda input validation and error handling

--- __tests__/createItem/function.int.js
+++ __tests__/createItem/function.int.js
@@ -5,12 +5,33 @@ axios.defaults.baseURL = `https://${process.env.httpApiGatewayEndpointId}.execut
 describe('createItem function', () => {
   it('should respond with statusCode 200 to correct request', async () => {
     // GIVEN
-    const payload = {}
+    const payload = {
+      a: 10,
+      b: 5,
+      method: 'add'
+    }
 
     // WHEN
-    const actual = await axios.post('/item', { data: payload })
+    const actual = await axios.post('/item', payload)
 
     // THEN
     expect(actual.status).toBe(200)
+  })
+
+  it('should respond with Bad Request 400 to incorrect request', async () => {
+    // GIVEN
+    const wrongPayload = {}
+
+    // WHEN
+    let actual
+    try {
+      await axios.post('/item', wrongPayload)
+    } catch (e) {
+      actual = e.response
+    }
+
+    // THEN
+    expect(actual.status).toBe(400)
+    expect(actual.statusText).toBe('Bad Request')
   })
 })
--- src/createItem/function.js
+++ src/createItem/function.js
@@ -1,11 +1,32 @@
 const middy = require('@middy/core')
+const validator = require('@middy/validator')
 const { autoProxyResponse } = require('middy-autoproxyresponse')
+const httpJsonBodyParser = require('@middy/http-json-body-parser')
+const httpErrorHandler = require('@middy/http-error-handler')
 
 // eslint-disable-next-line no-unused-vars
 const handler = async (event, context) => {
-  console.log('test');
-  return true
+  console.log('body', event.body);
+  return event.body
+}
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    body: {
+      type: 'object',
+      required: ['a', 'b', 'method'],
+      properties: {
+        a: { type: 'number' },
+        b: { type: 'number' },
+        method: { type: 'string' }
+      }
+    }
+  }
 }
 
 module.exports.handler = middy(handler)
+  .use(httpJsonBodyParser())
+  .use(validator({ inputSchema }))
+  .use(httpErrorHandler())
   .use(autoProxyResponse())
