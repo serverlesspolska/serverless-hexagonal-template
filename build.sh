137ad113c1b5f09b907210bad8051c6f11695b13
Pawel Zubkiewicz <pawel@serverlesspolska.pl>
2025-06-22 19:49:55 +0200

    fix: standardize ajv command options for newer version; update package dependencies for improved compatibility

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@
 
 bundle () {
   ajv validate ${1} --valid \
-    --strict true --coerce-types array --all-errors true --use-defaults empty
+    --strict=true --coerce-types=array --all-errors --use-defaults=empty
   ajv transpile ${1} \
-  --strict true --coerce-types array --all-errors true --use-defaults empty -o ${1%.json}.mjs
+  --strict=true --coerce-types=array --all-errors --use-defaults=empty -o ${1%.json}.mjs
 }
 
 for file in src/*/schema.*.json; do
